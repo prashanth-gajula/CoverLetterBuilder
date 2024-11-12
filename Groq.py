79d2ab478bc1481ae28281ea0523f1576c7c58bb
Prashanth <kumarprashanth782@gmail.com>
2024-11-11 20:45:43 -0600

    generated cover letter

--- Groq.py
+++ Groq.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 from langchain_groq import ChatGroq
+from langchain_core.prompts import PromptTemplate
 
 load_dotenv()
 
@@ -10,5 +11,18 @@ llm = ChatGroq(
     api_key = os.getenv("GROQ_API_KEY")
 )
 
-response = llm.invoke("Why latency needs to be reduced in AI applications?")
-print(response.content)
+def Generate_Cover_Letter(Uploaded_pdf):
+    
+    #Prompt for LLM
+    
+    Input_prompt  = PromptTemplate.from_template("""
+    Write a cover letter for a job application based on the resume: {Uploaded_pdf}
+    
+    """)
+    #print("welcome to the project")
+    chain_llm = Input_prompt | llm
+    response = chain_llm.invoke(input={Uploaded_pdf})
+    #print(response)
+    return response
+#response = llm.invoke("Why latency needs to be reduced in AI applications?")
+#print(response.content)
