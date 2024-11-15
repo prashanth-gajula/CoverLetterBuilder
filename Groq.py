15797bf75287125063bc421a7121113f04e9f113
Prashanth <kumarprashanth782@gmail.com>
2024-11-14 20:55:01 -0600

    modified the code to accepct the resume and additional instructions to generate a better cover letter

--- Groq.py
+++ Groq.py
@@ -11,17 +11,22 @@ llm = ChatGroq(
     api_key = os.getenv("GROQ_API_KEY")
 )
 
-def Generate_Cover_Letter(Uploaded_pdf):
+def Generate_Cover_Letter(Uploaded_pdf,Instructions):
     
     #Prompt for LLM
     
     Input_prompt  = PromptTemplate.from_template("""
-    Write a cover letter for a job application based on the resume: {Uploaded_pdf}
-    
+    Write a cover letter for a job application based on the resume: {Uploaded_pdf} and user instructions {Instructions}
+    If the user mentioned about the organization that he is going to apply and if there are any projects that are related to 
+    organization domain please talk about that project in the cover letter.
+    For Example: if the user applying for a job in chevron since it is a oil and gas company please mention about 
+    any petroleum project available in  resume and similarly for other organizations in other domain and 
+    if you are unable to determine organization domain talk about the users ability for the job role base on the projects and experience available in the resume.
+    (NO PREAMBLE) 
     """)
     #print("welcome to the project")
     chain_llm = Input_prompt | llm
-    response = chain_llm.invoke(input={Uploaded_pdf})
+    response = chain_llm.invoke(input={"Uploaded_pdf":Uploaded_pdf,"Instructions":Instructions})
     #print(response)
     return response
 #response = llm.invoke("Why latency needs to be reduced in AI applications?")
